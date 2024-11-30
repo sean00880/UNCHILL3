8bf5ed9f183179b9e18cb79f5f6db34e0a7a1d0b
sean00880 <shantanu1997@gmail.com>
2024-11-29 21:23:05 -0800

    11

--- src/components/LandingLayout.tsx
+++ src/components/LandingLayout.tsx
@@ -1,16 +1,11 @@
 // src/components/LandingLayout.tsx
 "use client";
-import React, { useState, useEffect } from 'react';
 import TopBar from './TopBar2';
 
 const LandingLayout: React.FC<{ children: React.ReactNode }> = ({ children }) => {
-  const [isDarkMode, setIsDarkMode] = useState(true);
 
-  const toggleTheme = () => setIsDarkMode(!isDarkMode);
 
-  useEffect(() => {
-    document.documentElement.setAttribute('data-theme', isDarkMode ? 'dark' : 'light');
-  }, [isDarkMode]);
+
 
   return (
     <div className="flex flex-col min-h-screen">
